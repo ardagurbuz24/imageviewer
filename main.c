7e4220cfa99d3eed06721a13ef1e925bb448ed68
ardagurbuz24 <gurbuz.arda@outlook.com>
2026-01-17 02:44:35 +0300

    modernized init

--- main.c
+++ main.c
@@ -2,13 +2,35 @@
 #include <stdlib.h>
 #include <SDL2/SDL.h>
 
-int main()
-{
-    SDL_Window *iwindow = SDL_CreateWindow("window", SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED, 600, 600, 0);
-    SDL_Surface *isurface = SDL_GetWindowSurface(iwindow);
+int main(){
 
-    SDL_FillRect(isurface, NULL, 0xFFFFFFFF);
-    SDL_UpdateWindowSurface(iwindow);
-    SDL_Delay(4000);
+    if(SDL_Init((SDL_INIT_VIDEO) < 0)) {
+        printf("sdl error: %s", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window *iwindow = SDL_CreateWindow("window", SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED, 600, 600, 0);
+    SDL_Renderer *irenderer = SDL_CreateRenderer(iwindow, -1, 0);
+
+    int running = 1;
+    SDL_Event event;
+
+    while(running) {
+        while(SDL_PollEvent(&event)) {
+            if(event.type == SDL_QUIT) {
+                running = 0;
+            }
+        }
+
+        SDL_SetRenderDrawColor(irenderer, 255, 255, 255, 255);
+        SDL_RenderClear(irenderer);
+
+        SDL_RenderPresent(irenderer);
+    }
+
+    SDL_DestroyRenderer(irenderer);
+    SDL_DestroyWindow(iwindow);
+    SDL_Quit();
+
     return 0;
 }
